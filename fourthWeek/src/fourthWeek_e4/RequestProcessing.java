612a18af830bbcdd52312686e6c91f44dbf7c4da
BlackPegasus9 <65912116+BlackPegasus9@users.noreply.github.com>
2021-10-19 11:48:27 +0700

    Bai 4

---
+++ fourthWeek/src/fourthWeek_e4/RequestProcessing.java
@@ -0,0 +1,83 @@
+package fourthWeek_e4;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class RequestProcessing extends Thread{  
+
+    String choice=null;
+    Socket s=null;
+    InputStream is = null;
+	OutputStream os = null;
+	DataInputStream dis = null;
+	DataOutputStream dos = null;
+
+    public RequestProcessing(Socket s){
+        this.s=s;
+    }
+
+    public void run() {
+    try{
+    	is = s.getInputStream();
+		os = s.getOutputStream();
+		dis = new DataInputStream(is);
+		dos = new DataOutputStream(os);
+
+    }catch(IOException e){
+        System.out.println("IO error in request processing.");
+    }
+
+    try {
+    	String choice = dis.readUTF();
+        while(true){
+        	switch(choice) {
+			case "1":
+				LocalTime time = LocalTime.now();
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+				String Time = time.format(formatter);
+				dos.writeUTF(Time);
+				break;
+			case "2":
+				LocalDate date = LocalDate.now();
+				DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				String Date = date.format(formatter1);
+				dos.writeUTF(Date);		
+				break;
+			case "3":
+				LocalDateTime dateTime = LocalDateTime.now();
+				DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+				String DateTime = dateTime.format(formatter2);
+				dos.writeUTF(DateTime);
+				break;	
+			default:
+				break;
+				}
+        	return;
+        	}   
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    	 return;
+	    }finally{    
+	    try{
+	        System.out.println("Connection Closing..");
+	        if (s!=null){
+	        s.close();
+	        System.out.println("Socket Closed");
+	        return;
+	        }
+	       }
+	    catch(IOException ie){
+	        System.out.println("Socket Close Error");
+	        return;
+	    	}
+	    }
+    }
+}
---
+++ fourthWeek/src/fourthWeek_e4/TimeServer.java
@@ -0,0 +1,40 @@
+package fourthWeek_e4;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class TimeServer {
+public static void main(String args[]){
+
+
+    Socket s = null;
+    ServerSocket ss = null;
+    try{
+        ss = new ServerSocket(2345);
+        System.out.println("Server is created.");
+    }
+    catch(IOException e){
+    System.out.println("Server error: " + e);
+
+    }
+
+    while(true){
+        try{
+            s= ss.accept();
+            System.out.println("Connect Successfully.");
+            RequestProcessing rp=new RequestProcessing(s);
+            rp.start();         
+        }
+    catch(IOException e){
+        e.printStackTrace();
+        System.out.println("Connection Error");
+        return;
+    }
+    }
+
+}
+
+}
+
